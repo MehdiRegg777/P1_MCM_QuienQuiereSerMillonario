f22eb6333b7b5647c3d2d03437a83c7133088975
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-19 18:35:54 +0200

    Spect 4

---
+++ Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>¿Quién quiere ser millonario?</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body class="fondo">
+    <div class="container">
+        <h1>¿Quién quiere ser millonario?</h1>
+        <p class="instructions">Instrucciones del juego:</p>
+        <ul>
+            <li>Debes responder correctamente una serie de preguntas para ganar dinero.</li>
+            <li>Cuanto más niveles pases, se complicarán las preguntas.</li>
+        </ul>
+        <a class="play-button" href="game.php">¡JUGAR!</a>
+    </div>
+</body>
+</html>
