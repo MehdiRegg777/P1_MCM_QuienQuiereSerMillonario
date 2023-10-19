e7ff1d5e1e0fffef2e5e164ad14013edcc64de50
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-19 20:12:41 +0200

    lose

---
+++ lose.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>¿Quién quiere ser millonario?</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body class="fondo" >
+    <header>
+        <h1>¿Quién quiere ser millonario?</h1>
+    </header>
+    <div class="container">
+        
+        <h2>Lo sentimos mucho!!</h2>
+    
+        <a class="play-button" href="index.php">¡Volver al inicio!</a>
+    </div>
+    <footer>
+    <p>© 2023 Tu Nombre</p>
+    </footer>
+
+</body>
+</html>
