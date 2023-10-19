1ef58d5551696d4a19cd7b3828d71a1b3b1325af
Marcelogr04 <marcelogr2004@gmail.com>
2023-10-19 19:11:53 +0200

    hola

--- game.php
+++ game.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <?php
-    $question = file()
+    $questions_3 = file('questions/catalan_1.txt', FILE_IGNORE_NEW_LINES);
+    $selectedQuestions = array_rand($questions_3, 3);
+
+    // Utiliza el índice obtenido para acceder a las preguntas seleccionadas
+    echo '<h2>' . $questions_3[$selectedQuestions[0]] . '</h2>';
+    echo '<h2>' . $questions_3[$selectedQuestions[1]] . '</h2>';
+    echo '<h2>' . $questions_3[$selectedQuestions[2]] . '</h2>';
+
+    echo '<form action="game.php" method="post">';
     ?>
 </body>
 </html>
