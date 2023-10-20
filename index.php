55790f073c334aef1ffd4d4f320bdb54d0cf4e22
cl6mh7 <claudiamoyamo@gmail.com>
2023-10-20 18:34:06 +0200

    Second commit

--- index.php
+++ index.php
@@ -1,39 +1,72 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
     <head>
         <title>¿Quién quiere ser millonario?</title>
+        <meta author="" content="Claudia, Mehdi i Marcelo (2n DAW)">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="style.css" rel="stylesheet">
         <link rel="shortcut icon" href="imgs/logo.png" />
     </head>
-    <body class="fondo">
+    <body>
         <header>
             <h1>¿Quién quiere ser millonario?</h1>
         </header>
 
         <div class="container">    
-            <h2>Bienvenido</h2>
+            <h2><strong>Bienvenido</strong></h2>
             <h3><em>Instrucciones del juego</em></h3>
+        
+            <p>El juego en línea de <em>¿Quién quiere ser millonario?</em> funciona tal como el exitoso programa de la vida real.
+        
+            <br /><br />Antes de empezar, hay que comprender que el juego en línea contiene un total de seis dificultades, cada una repartidas en tres preguntas, por
+            lo consecuente hay 18 preguntas en total. Las dificultades son: muy fácil, fácil, medio, versado, difícil y muy difícil.
+            El juego comienza con una pregunta del primer nivel de dificultad. Si respondes correctamente, se mostrará la siguiente pregunta, y
+            así hasta la tercera. Si pasas la primera ronda, se aumentará la dificultad. El juego SIEMPRE cambia la dificultad automáticamente después de
+            que el usuario responda la tercera pregunta de la tanda.<br/><br /></p>
             
-            <ul>
-                <li>Debes responder correctamente una serie de preguntas para ganar dinero.</li>
-                <li>Cuanto más niveles pases, se complicarán las preguntas.</li>
-            </ul>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Id donec ultrices tincidunt arcu non. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Pharetra vel turpis nunc eget lorem dolor sed. Aliquet nibh praesent tristique magna sit amet. In cursus turpis massa tincidunt dui ut ornare lectus sit. Quis hendrerit dolor magna eget est lorem ipsum. Facilisis gravida neque convallis a cras semper auctor neque. Fermentum dui faucibus in ornare quam viverra. Vitae elementum curabitur vitae nunc sed. Aliquet nibh praesent tristique magna sit amet purus. Id leo in vitae turpis massa sed elementum. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. A diam maecenas sed enim ut sem viverra aliquet. Pellentesque id nibh tortor id. Mattis rhoncus urna neque viverra justo. Auctor urna nunc id cursus metus aliquam. Rhoncus mattis rhoncus urna neque. Et odio pellentesque diam volutpat. Nec ultrices dui sapien eget mi proin sed libero enim. Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Non consectetur a erat nam at lectus urna. Eu augue ut lectus arcu bibendum at varius vel pharetra.</p>
+            <h3><em>¿Cómo empezar a jugar?</em></hr>
+            
+            <p>Primero, tienes la opción de escoger el idioma en el que quieres que se muestre toda la página debajo de esta explicación. Cuando lo hayas escogido,
+            debes clicar en el botón <em>Jugar</em> —se situa debajo de esta explicación— para poder iniciar tu partida. No se creará un usuario al menos que
+            especifiques que sí quieres y que das el consentimiento a subir tu nombre de usuario, junto a tu ID de jugador y los puntos que has sacado para publicarlo
+            en la tabla de clasificaciones que se muestra al clicar en el botón <em>Hall of fame</em> o cuando acaba la partida (tanto si has ganado como si has perdido).
         
-            <a class="play-button" href="game.php">Jugar</a>
-            <a class="halloffame-button" href="game.php">Hall of fame</a>
-        </div>
+            <br /><br />Lo primero que verás es una página nueva con la primera pregunta. Deberás clicar a la respuesta que creas correcta y la página te 
+            mostrará si has acertado o si te has equivocado.</p>
 
-        <div class="languagesimages">
-            <img src="imgs/españa.jpeg" alt="Imagen de la bandera de España">
-            <img src="imgs/catalunya.jpeg" alt="Imagen de la bandera de Catalunya">
-            <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido">
+            <ul>
+                <li>Si te has equivocado, ¡no te preocupes!, la página te mostrará un botón para volver al inicio y puedes volver a jugar siempre que quieras.</li>
+                <li>Si has acertado, ¡enhorabuena!, aparecerá en la página la siguiente pregunta. Y así hasta completar las seis dificultades.</li>
+            </ul>
+
+            <p>Si has ganado, ¡se mostrará una nueva pantalla donde se te felicitará por tu gran hazaña y se mostrará la cantidad de dinero que has ganado! También
+                saldrá la tabla de clasificaciones. Antes de esto te preguntaremos si quieres publicar tus datos antes mencionados (nombre de usuario, ID de usuario
+                y puntuación). Jamás publicaremos tus datos sin consentimiento.</p>
+            
+            <a class="play-button" href="game.php"><em>Jugar</em></a>
+            <a class="halloffame-button" href="game.php"><em>Hall of fame</em></a>
+
+            <div class="languagesimages">
+                <img src="imgs/españa.jpeg" alt="Imagen de la bandera de España">
+                <img src="imgs/catalunya.jpeg" alt="Imagen de la bandera de Catalunya">
+                <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido">
+            </div>
+
+            <div class="presentationimage">
+                <img src="imgs/netflix-millonariopng.jpg" alt="Escena del juego '¿Quién quiere ser millonario' Sale el presentador y un jugador.">
+            </div>
+
         </div>
         
-        <footer>
+        <footer class="footerinfo">
             <p>© MCM S.A.</p>
-            <p></p>
-            <p></p>
+            <p>Contact us</p>
+            <p>Follow us</p>
+            <p>empresa@domini.cat</p>
+            <p>twt ig p</p>
+        </div>
         </footer>
 
     </body>
