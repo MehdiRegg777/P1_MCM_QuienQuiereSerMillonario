c4fc125d252287099fc4b839e792257b3bb5d3a2
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-20 16:04:51 +0200

    spect 5 y 6

--- game.php
+++ game.php
@@ -1,21 +1,108 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game</title>
+    <style>
+        .question-box {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin: 10px;
+        }
+        .question-box.disabled {
+            pointer-events: none;
+            opacity: 0.6;
+        }
+    </style>
 </head>
 <body>
-    <?php
-    $questions_3 = file('questions/catalan_1.txt', FILE_IGNORE_NEW_LINES);
-    $selectedQuestions = array_rand($questions_3, 3);
+<div class="question-box">
+    
+<?php
+session_start();
 
-    // Utiliza el índice obtenido para acceder a las preguntas seleccionadas
-    echo '<h2>' . $questions_3[$selectedQuestions[0]] . '</h2>';
-    echo '<h2>' . $questions_3[$selectedQuestions[1]] . '</h2>';
-    echo '<h2>' . $questions_3[$selectedQuestions[2]] . '</h2>';
+// Función para obtener una pregunta aleatoria
+function obtenerPreguntaAleatoria($preguntas) {
+    return $preguntas[array_rand($preguntas)];
+}
 
-    echo '<form action="game.php" method="post">';
-    ?>
+if (!isset($_SESSION['preguntas'])) {
+    // Leer el contenido del archivo y dividirlo en preguntas
+    $contenido = file_get_contents('questions/spanish_1.txt');
+    $lineas = explode("\n", $contenido);
+
+    $preguntas = array();
+
+    for ($i = 0; $i < count($lineas); $i += 5) {
+        $pregunta = trim(substr($lineas[$i], 2)); // Extraer la pregunta
+        $respuestas = array_map('trim', array_slice($lineas, $i + 1, 4)); // Extraer las respuestas
+
+        // Eliminar símbolos * - +
+        $respuestas = array_map(function($respuesta) {
+            return str_replace(['*', '-', '+'], '', $respuesta);
+        }, $respuestas);
+
+        // Agregar la pregunta y respuestas al arreglo
+        $preguntas[] = array(
+            "pregunta" => $pregunta,
+            "respuestas" => $respuestas
+        );
+    }
+
+    $_SESSION['preguntas'] = $preguntas;
+}
+
+// Obtén la pregunta actual de la sesión
+if (isset($_SESSION['pregunta_actual'])) {
+    $preguntaAleatoria = $_SESSION['pregunta_actual'];
+} else {
+    $preguntaAleatoria = obtenerPreguntaAleatoria($_SESSION['preguntas']);
+    $_SESSION['pregunta_actual'] = $preguntaAleatoria;
+}
+function printarPregunta($preguntaAleatoria) {
+    // Mostrar la pregunta y respuestas
+    echo "<div class='question-box'>";
+    echo "<h2>{$preguntaAleatoria['pregunta']}</h2>";
+    foreach ($preguntaAleatoria['respuestas'] as $key => $respuesta) {
+        echo "<input type='radio' name='respuesta' value='$respuesta' id='respuesta$key'> <label for='respuesta$key'>$respuesta</label><br>";
+    }
+    echo "<input type='hidden' name='pregunta' value='" . $preguntaAleatoria['pregunta'] . "'>";
+    echo "<button onclick='comprobarRespuesta()'>Seleccionar Respuesta</button>";
+    echo "</div>";
+}
+
+printarPregunta($preguntaAleatoria);
+
+?>
+<script>
+    function comprobarRespuesta() {
+        const respuestaCorrecta = '<?php echo $preguntaAleatoria['respuestas'][0]; ?>';
+        console.log(respuestaCorrecta);
+        const respuestaSeleccionada = document.querySelector('input[name="respuesta"]:checked');
+        if (respuestaSeleccionada) {
+            if (respuestaSeleccionada.value === respuestaCorrecta) {
+                alert('¡Felicidades! Respuesta correcta.');
+
+                // Deshabilitar la pregunta actual
+                const preguntaActual = document.querySelector('.question-box');
+                preguntaActual.classList.add('disabled');
+
+                // Limpiar la pregunta actual de la sesión
+                <?php unset($_SESSION['pregunta_actual']); ?>
+
+                // Redirigir al usuario al inicio
+            } else {
+                alert('Respuesta incorrecta. La respuesta correcta era: ' + respuestaCorrecta);
+                // Redirigir al usuario al inicio
+                window.location.href = 'index.php';
+            }
+        } else {
+            alert('Por favor, selecciona una respuesta.');
+        }
+    }
+</script>
 </body>
 </html>
