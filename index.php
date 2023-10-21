8f240ebf178ebb7de9532e812098a1a7579cc2bf
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2023-10-21 17:33:09 +0200

    canvios

--- index.php
+++ index.php
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>¿Quién quiere ser millonario?</title>
-        <link href="style.css" rel="stylesheet">
-        <link rel="shortcut icon" href="imgs/logo.png" />
-    </head>
-    <body class="fondo">
-        <header>
-            <h1>¿Quién quiere ser millonario?</h1>
-        </header>
-
-        <div class="container">    
-            <h2>Bienvenido</h2>
-            <h3><em>Instrucciones del juego</em></h3>
-            
-            <ul>
-                <li>Debes responder correctamente una serie de preguntas para ganar dinero.</li>
-                <li>Cuanto más niveles pases, se complicarán las preguntas.</li>
-            </ul>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Id donec ultrices tincidunt arcu non. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Pharetra vel turpis nunc eget lorem dolor sed. Aliquet nibh praesent tristique magna sit amet. In cursus turpis massa tincidunt dui ut ornare lectus sit. Quis hendrerit dolor magna eget est lorem ipsum. Facilisis gravida neque convallis a cras semper auctor neque. Fermentum dui faucibus in ornare quam viverra. Vitae elementum curabitur vitae nunc sed. Aliquet nibh praesent tristique magna sit amet purus. Id leo in vitae turpis massa sed elementum. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. A diam maecenas sed enim ut sem viverra aliquet. Pellentesque id nibh tortor id. Mattis rhoncus urna neque viverra justo. Auctor urna nunc id cursus metus aliquam. Rhoncus mattis rhoncus urna neque. Et odio pellentesque diam volutpat. Nec ultrices dui sapien eget mi proin sed libero enim. Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Non consectetur a erat nam at lectus urna. Eu augue ut lectus arcu bibendum at varius vel pharetra.</p>
-        
-            <a class="play-button" href="game.php?nuevo_juego=true">Jugar</a>
-            <a class="halloffame-button" href="game.php">Hall of fame</a>
-        </div>
-
-        <div class="languagesimages">
-            <img src="imgs/españa.jpeg" alt="Imagen de la bandera de España">
-            <img src="imgs/catalunya.jpeg" alt="Imagen de la bandera de Catalunya">
-            <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido">
-        </div>
-        
-        <footer>
-            <p>© MCM S.A.</p>
-            <p></p>
-            <p></p>
-        </footer>
-
-    </body>
-</html>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <html>
@@ -113,4 +71,3 @@
 
     </body>
 </html>
->>>>>>> origin/CMoyano
