c7447c370354aac0beb1c1407b07cc68e285bfa5
Marcelogr04 <marcelogr2004@gmail.com>
2023-10-24 17:57:23 +0200

    language

--- game.php
+++ game.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,6 +13,7 @@
     </head>
 
     <body>
+        <div id="spanish">
         <header>
             <h1>¿Quién quiere ser millonario?</h1>
         </header>
@@ -17,14 +21,24 @@
         <?php
         
         if (isset($_GET['niveles'])) {
-            $_SESSION['nivel'] = intval($_GET['niveles']);
+            $_GET['nivel'] = intval($_GET['niveles']);
         } else {
-            $_SESSION['nivel'] = 1;
+            $_GET['nivel'] = 1;
         }
 
-        $nivel_actual = $_SESSION['nivel'];
+        $nivel_actual = $_GET['nivel'];
 
-        if (!isset($_SESSION['preguntas']) || isset($_GET['nuevo_juego'])) {
+        /* if ($nivel_actual === 1) {
+            if (isset($_POST['language'])) {
+                $language = $_POST['language']; // Obtener el idioma seleccionado
+            }
+            $_SESSION['language'] = $language;
+        }
+        $_GET['selectedLanguage'] = $_SESSION['language'];
+        echo "<h2>{$_GET['selectedLanguage']}</h2>";
+        echo "<h2>{$nivel_actual}</h2>";
+        echo "<h2>{$_SESSION['language']}</h2>"; */
+        if (!isset($_GET['preguntas']) || isset($_GET['nuevo_juego'])) {
             $contenido = file_get_contents("questions/spanish_$nivel_actual.txt");
             $lineas = explode("\n", $contenido);
             $preguntas = array();
@@ -47,18 +61,18 @@
             }
 
             shuffle($preguntas);
-            $_SESSION['preguntas'] = $preguntas;
-            $_SESSION['pregunta_actual'] = 0;
+            $_GET['preguntas'] = $preguntas;
+            $_GET['pregunta_actual'] = 0;
         }
 
-        $preguntas = $_SESSION['preguntas'];
+        $preguntas = $_GET['preguntas'];
         
         foreach ($preguntas as $key => $pregunta) {
             if ($key >= 3) {
                 break;
             }
 
-            $claseRespuesta = $key <= $_SESSION['pregunta_actual'] ? '' : 'bloqueada';
+            $claseRespuesta = $key <= $_GET['pregunta_actual'] ? '' : 'bloqueada';
             echo "<div class='pregunta $claseRespuesta' id='pregunta" . $key . "'>";
             
             // AQUÍ LA PREGUNTA.
@@ -75,49 +89,188 @@
             echo "</div>";
         }
         
-        $nivels = $_SESSION['nivel'];
+        $nivels = $_GET['nivel'];
         $nivels++;
         echo "<div class='ghof-buttons'>";
         echo "<button id='inicio-btn' onclick='regresarAlInicio()' style='display: none;' ><em><strong>Volver al inicio</em></strong></button>";
         echo "<button id='next-question' onclick='nextQuestion($nivels)' style='display: none;' >Siguiente pregunta</button>";
         echo "</div>"
         ?>
+        </div>
+        <div id="catalan" style="display: none;">
+        <header>
+            <h1>Qui vol ser milionari?</h1>
+        </header>
+        
+        <?php
+        
+        if (isset($_GET['niveles'])) {
+            $_GET['nivel'] = intval($_GET['niveles']);
+        } else {
+            $_GET['nivel'] = 1;
+        }
+
+        $nivel_actual = $_GET['nivel'];
+
+        /* if ($nivel_actual === 1) {
+            if (isset($_POST['language'])) {
+                $language = $_POST['language']; // Obtener el idioma seleccionado
+            }
+            $_SESSION['language'] = $language;
+        }
+        $_GET['selectedLanguage'] = $_SESSION['language'];
+        echo "<h2>{$_GET['selectedLanguage']}</h2>";
+        echo "<h2>{$nivel_actual}</h2>";
+        echo "<h2>{$_SESSION['language']}</h2>"; */
+        if (!isset($_GET['preguntas']) || isset($_GET['nuevo_juego'])) {
+            $contenido = file_get_contents("questions/catalan_$nivel_actual.txt");
+            $lineas = explode("\n", $contenido);
+            $preguntas = array();
+
+            for ($i = 0; $i < count($lineas); $i += 5) {
+                $pregunta = trim(substr($lineas[$i], 1));
+                $respuestas = array_map('trim', array_slice($lineas, $i + 1, 4));
+
+                foreach ($respuestas as $posicion => $respuesta) {
+                    if (strpos($respuesta, '+') !== false) {
+                        $respuestaCorrecta = $posicion;
+                    }
+                }
+
+                $preguntas[] = array(
+                    "pregunta" => $pregunta,
+                    "respuestas" => $respuestas,
+                    "respuesta_correcta" => $respuestaCorrecta,
+                );
+            }
+
+            shuffle($preguntas);
+            $_GET['preguntas'] = $preguntas;
+            $_GET['pregunta_actual'] = 0;
+        }
+
+        $preguntas = $_GET['preguntas'];
+        
+        foreach ($preguntas as $key => $pregunta) {
+            if ($key >= 3) {
+                break;
+            }
+
+            $claseRespuesta = $key <= $_GET['pregunta_actual'] ? '' : 'bloqueada';
+            echo "<div class='pregunta $claseRespuesta' id='pregunta" . $key . "'>";
+            
+            // AQUÍ LA PREGUNTA.
+            echo "<h2 class = 'questiontitle'>{$pregunta['pregunta']}</h2>";
+            echo "<div id='respuesta $claseRespuesta'>";
+            
+            foreach ($pregunta['respuestas'] as $answerKey => $respuesta) {
+                $respuesta = str_replace(['+', '-', '*'], '', $respuesta);
+                echo "<div class='respuesta $claseRespuesta' data-pregunta='$key' data-respuesta='$answerKey' data-correcta='" . $pregunta['respuesta_correcta'] . "' id='respuesta-$key-$answerKey' onclick=\"seleccionarRespuesta('$key', '$answerKey')\">$respuesta</div>";
+            }
+            
+            echo "<button class='responder-btn' data-pregunta='$key' id='responder-btn-$key' disabled onclick=\"responderPregunta('$key', '$nivel_actual')\">Responder</button>";
+            echo "</div>";
+            echo "</div>";
+        }
+        
+        $nivels = $_GET['nivel'];
+        $nivels++;
+        echo "<div class='ghof-buttons'>";
+        echo "<button id='inicio-btn' onclick='regresarAlInicio()' style='display: none;' ><em><strong>Volver al inicio</em></strong></button>";
+        echo "<button id='next-question' onclick='nextQuestion($nivels)' style='display: none;' >Siguiente pregunta</button>";
+        echo "</div>"
+        ?>
+        </div>
+        <div id="english" style="display: none;">
+        <header>
+            <h1>Who wants to be a millionaire?</h1>
+        </header>
+        
+        <?php
+        
+        if (isset($_GET['niveles'])) {
+            $_GET['nivel'] = intval($_GET['niveles']);
+        } else {
+            $_GET['nivel'] = 1;
+        }
+
+        $nivel_actual = $_GET['nivel'];
+
+        /* if ($nivel_actual === 1) {
+            if (isset($_POST['language'])) {
+                $language = $_POST['language']; // Obtener el idioma seleccionado
+            }
+            $_SESSION['language'] = $language;
+        }
+        $_GET['selectedLanguage'] = $_SESSION['language'];
+        echo "<h2>{$_GET['selectedLanguage']}</h2>";
+        echo "<h2>{$nivel_actual}</h2>";
+        echo "<h2>{$_SESSION['language']}</h2>"; */
+        if (!isset($_GET['preguntas']) || isset($_GET['nuevo_juego'])) {
+            $contenido = file_get_contents("questions/spanish_$nivel_actual.txt");
+            $lineas = explode("\n", $contenido);
+            $preguntas = array();
+
+            for ($i = 0; $i < count($lineas); $i += 5) {
+                $pregunta = trim(substr($lineas[$i], 1));
+                $respuestas = array_map('trim', array_slice($lineas, $i + 1, 4));
+
+                foreach ($respuestas as $posicion => $respuesta) {
+                    if (strpos($respuesta, '+') !== false) {
+                        $respuestaCorrecta = $posicion;
+                    }
+                }
+
+                $preguntas[] = array(
+                    "pregunta" => $pregunta,
+                    "respuestas" => $respuestas,
+                    "respuesta_correcta" => $respuestaCorrecta,
+                );
+            }
+
+            shuffle($preguntas);
+            $_GET['preguntas'] = $preguntas;
+            $_GET['pregunta_actual'] = 0;
+        }
+
+        $preguntas = $_GET['preguntas'];
+        
+        foreach ($preguntas as $key => $pregunta) {
+            if ($key >= 3) {
+                break;
+            }
+
+            $claseRespuesta = $key <= $_GET['pregunta_actual'] ? '' : 'bloqueada';
+            echo "<div class='pregunta $claseRespuesta' id='pregunta" . $key . "'>";
+            
+            // AQUÍ LA PREGUNTA.
+            echo "<h2 class = 'questiontitle'>{$pregunta['pregunta']}</h2>";
+            echo "<div id='respuesta $claseRespuesta'>";
+            
+            foreach ($pregunta['respuestas'] as $answerKey => $respuesta) {
+                $respuesta = str_replace(['+', '-', '*'], '', $respuesta);
+                echo "<div class='respuesta $claseRespuesta' data-pregunta='$key' data-respuesta='$answerKey' data-correcta='" . $pregunta['respuesta_correcta'] . "' id='respuesta-$key-$answerKey' onclick=\"seleccionarRespuesta('$key', '$answerKey')\">$respuesta</div>";
+            }
+            
+            echo "<button class='responder-btn' data-pregunta='$key' id='responder-btn-$key' disabled onclick=\"responderPregunta('$key', '$nivel_actual')\">Responder</button>";
+            echo "</div>";
+            echo "</div>";
+        }
+        
+        $nivels = $_GET['nivel'];
+        $nivels++;
+        echo "<div class='ghof-buttons'>";
+        echo "<button id='inicio-btn' onclick='regresarAlInicio()' style='display: none;' ><em><strong>Volver al inicio</em></strong></button>";
+        echo "<button id='next-question' onclick='nextQuestion($nivels)' style='display: none;' >Siguiente pregunta</button>";
+        echo "</div>"
+        ?>
+        </div>
         
         <audio id="correctSound" src="mp3/correct.mp3"></audio>
         <audio id="incorrectSound" src="mp3/fail.mp3"></audio>
         <script src="funciomGame.js"></script>
+        <script src="funcionLanguage.js"></script>
 
-<<<<<<< HEAD
-        $claseRespuesta = $key <= $_SESSION['pregunta_actual'] ? '' : 'bloqueada';
-
-        echo "<div class='pregunta $claseRespuesta' id='pregunta" . $key . "'>";
-        echo "<h2>{$pregunta['pregunta']}</h2>";
-
-        echo "<div id='respuesta $claseRespuesta'>";
-        foreach ($pregunta['respuestas'] as $answerKey => $respuesta) {
-            $respuesta = str_replace(['+', '-', '*'], '', $respuesta);
-            echo "<div class='respuesta $claseRespuesta' data-pregunta='$key' data-respuesta='$answerKey' data-correcta='" . $pregunta['respuesta_correcta'] . "' id='respuesta-$key-$answerKey' onclick=\"seleccionarRespuesta('$key', '$answerKey')\">$respuesta</div>";
-        }
-        echo "<button class='responder-btn' data-pregunta='$key' id='responder-btn-$key' disabled onclick=\"responderPregunta('$key', '$nivel_actual')\">Responder</button>";
-        echo "</div>";
-        echo "</div>";
-    }
-    $nivels = $_SESSION['nivel'];
-    $nivels++;
-    echo "<button id='inicio-btn' onclick='regresarAlInicio()' style='display: none;' >Volver al inicio</button>";
-    echo "<button id='next-question' onclick='nextQuestion($nivels)' style='display: none;' >Siguiente pregunta</button>";
-    // echo "<pre>";
-    // print_r($_SESSION);
-    // echo "</pre>";
-
-    ?>
-    <audio id="correctSound" src="mp3/correct.mp3"></audio>
-    <audio id="incorrectSound" src="mp3/fail.mp3"></audio>
-    <script src="funciomGame.js"></script>
-    <script src="funcionLanguage.js"></script>
-
-</body>
-=======
         <footer class="footerinfo">
             <p>© MCM S.A.</p>
             <p>Contact us</p>
@@ -125,7 +278,6 @@
             <p>empresa@domini.cat</p>
             <p>twt ig p</p>
         </footer>
->>>>>>> origin/Mehdi
 
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 <?php
 session_start();
+$_SESSION['language'] = $_GET['lang'] ?? 'spanish';
+$selected_language = $_SESSION['selected_language'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -60,7 +61,7 @@ session_start();
                 <div class="languagesimages">
                     <img src="imgs/españa.jpeg" alt="Imagen de la bandera de España" onclick="changeLanguage('spanish')">
                     <img src="imgs/catalunya.jpeg" alt="Imagen de la bandera de Catalunya" onclick="changeLanguage('catalan')">
-                    <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido" onclick="changeLanguage('english')">
+                    <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido"  onclick="changeLanguage('english')">
                 </div>
 
                 <div class="presentationimage">
@@ -68,7 +69,6 @@ session_start();
                 </div>
             </div>
         </div>
-
         <div id="catalan" style="display: none;">
             <header>
                 <h1>Qui vol ser milionari?</h1>
@@ -109,7 +109,7 @@ session_start();
                     <form action="game.php" method="post">
                         <input type="hidden" name="language" value="catalan">
                         <button class="play-button" type="submit">Jugar</button>
-                        <a class="halloffame-button" href="game.php">Hall of fame</a>
+                        <a class="halloffame-button" href="game.php"><em>Hall of fame</em></a>
                     </form>
                 </div>
                 <div class="languagesimages">
@@ -123,10 +123,9 @@ session_start();
                 </div>
             </div>
         </div>
-
         <div id="english" style="display: none;">
             <header>
-                <h1>Who Wants to Be a Millionaire?</h1>
+                <h1>Who wants to be a millionaire?</h1>
             </header>
 
             <div class="container">
@@ -162,8 +161,8 @@ session_start();
                 <div class="ghof-buttons">
                     <form action="game.php" method="post">
                         <input type="hidden" name="language" value="english">
-                        <button class="play-button" type="submit">Play</button>
-                        <a class="halloffame-button" href="game.php"><em>Hall of Fame</em></a>
+                        <button class="play-button" type="submit">Jugar</button>
+                        <a class="halloffame-button" href="game.php"><em>Hall of fame</em></a>
                     </form>
                 </div>
                 <div class="languagesimages">
@@ -177,80 +176,6 @@ session_start();
                 </div>
             </div>
         </div>
-        
-        <footer class="footerinfo">
-            <p>© MCM S.A.</p>
-            <p>Contact us</p>
-            <p>Follow us</p>
-            <p>empresa@domini.cat</p>
-            <p>twt ig p</p>
-        </footer>
-        <script src="funcionLanguage.js"></script>
-    </body>
-</html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<html>
-    <head>
-        <title>¿Quién quiere ser millonario?</title>
-        <meta author="" content="Claudia, Mehdi i Marcelo (2n DAW)">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style.css" rel="stylesheet">
-        <link rel="shortcut icon" href="imgs/logo.png" />
-    </head>
-    <body>
-        <header>
-            <h1>¿Quién quiere ser millonario?</h1>
-        </header>
-
-        <div class="container">    
-            <h2><strong>Bienvenido</strong></h2>
-            <h3><em>Instrucciones del juego</em></h3>
-        
-            <p>El juego en línea de <em>¿Quién quiere ser millonario?</em> funciona tal como el exitoso programa de la vida real.
-        
-            <br /><br />Antes de empezar, hay que comprender que el juego en línea contiene un total de seis dificultades, cada una repartidas en tres preguntas, por
-            lo consecuente hay 18 preguntas en total. Las dificultades son: muy fácil, fácil, medio, versado, difícil y muy difícil.
-            El juego comienza con una pregunta del primer nivel de dificultad. Si respondes correctamente, se mostrará la siguiente pregunta, y
-            así hasta la tercera. Si pasas la primera ronda, se aumentará la dificultad. El juego SIEMPRE cambia la dificultad automáticamente después de
-            que el usuario responda la tercera pregunta de la tanda.<br/><br /></p>
-            
-            <h3><em>¿Cómo empezar a jugar?</em></hr>
-            
-            <p>Primero, tienes la opción de escoger el idioma en el que quieres que se muestre toda la página debajo de esta explicación. Cuando lo hayas escogido,
-            debes clicar en el botón <em>Jugar</em> —se situa debajo de esta explicación— para poder iniciar tu partida. No se creará un usuario al menos que
-            especifiques que sí quieres y que das el consentimiento a subir tu nombre de usuario, junto a tu ID de jugador y los puntos que has sacado para publicarlo
-            en la tabla de clasificaciones que se muestra al clicar en el botón <em>Hall of fame</em> o cuando acaba la partida (tanto si has ganado como si has perdido).
-        
-            <br /><br />Lo primero que verás es una página nueva con la primera pregunta. Deberás clicar a la respuesta que creas correcta y la página te 
-            mostrará si has acertado o si te has equivocado.</p>
-
-            <ul>
-                <li>Si te has equivocado, ¡no te preocupes!, la página te mostrará un botón para volver al inicio y puedes volver a jugar siempre que quieras.</li>
-                <li>Si has acertado, ¡enhorabuena!, aparecerá en la página la siguiente pregunta. Y así hasta completar las seis dificultades.</li>
-            </ul>
-
-            <p>Si has ganado, ¡se mostrará una nueva pantalla donde se te felicitará por tu gran hazaña y se mostrará la cantidad de dinero que has ganado! También
-                saldrá la tabla de clasificaciones. Antes de esto te preguntaremos si quieres publicar tus datos antes mencionados (nombre de usuario, ID de usuario
-                y puntuación). Jamás publicaremos tus datos sin consentimiento.</p>
-            
-                <div class="ghof-buttons">
-                    <a class="play-button" href="game.php"><em>Jugar</em></a>
-                    <a class="halloffame-button" href="ranking.php"><em>Hall of fame</em></a>
-                </div>
-            <div class="languagesimages">
-                <img src="imgs/españa.jpeg" alt="Imagen de la bandera de España">
-                <img src="imgs/catalunya.jpeg" alt="Imagen de la bandera de Catalunya">
-                <img src="imgs/uk.webp" alt="Imagen de la bandera del Reino Unido">
-            </div>
-
-            <div class="presentationimage">
-                <img src="imgs/netflix-millonariopng.jpg" alt="Escena del juego '¿Quién quiere ser millonario' Sale el presentador y un jugador.">
-            </div>
-
-        </div>
         <audio id="start" autoplay>
             <source src="mp3/inicio.mp3" type="audio/mpeg">
         </audio>
@@ -262,6 +187,6 @@ session_start();
             <p>twt ig p</p>
         </footer>
         <script src="funcionPublish.js"></script>
+        <script src="funcionLanguage.js"></script>
     </body>
 </html>
->>>>>>> origin/Mehdi
