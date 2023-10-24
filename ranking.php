ce381d9c839ea84183089f4178c970ee371bca24
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-24 18:26:09 +0200

    commit

--- ranking.php
+++ ranking.php
@@ -13,69 +13,71 @@
         <header>
             <h1>¿Quién quiere ser millonario?</h1>
         </header>
+    <div class="container">    
         <h3>Clasificaciones</h3><br>
-    <table border="1" id="correctqueststable">
-    <tr>
-        <th>Nombre</th>
-        <th>Puntos</th>
-        <th>ID</th>
-    </tr>
+            <table border="1" id="correctqueststable">
+            <tr>
+                <th>Nombre</th>
+                <th>Puntos</th>
+                <th>ID</th>
+            </tr>
 
-    <?php
-        // Ruta del archivo de registros
-        $archivo = "records.txt";
+            <?php
+                // Ruta del archivo de registros
+                $archivo = "records.txt";
 
-        // Verifica si el archivo existe
-        if (file_exists($archivo)) {
-            // Abre el archivo para lectura
-            $handle = fopen($archivo, "r");
+                // Verifica si el archivo existe
+                if (file_exists($archivo)) {
+                    // Abre el archivo para lectura
+                    $handle = fopen($archivo, "r");
 
-            // Inicializa un array para almacenar los datos
-            $data = array();
+                    // Inicializa un array para almacenar los datos
+                    $data = array();
 
-            // Recorre el archivo línea por línea
-            while (($line = fgets($handle)) !== false) {
-                // Divide la línea en sus partes separadas por comas
-                $parts = explode(",", $line);
+                    // Recorre el archivo línea por línea
+                    while (($line = fgets($handle)) !== false) {
+                        // Divide la línea en sus partes separadas por comas
+                        $parts = explode(",", $line);
 
-                // Verifica que haya tres partes
-                if (count($parts) == 3) {
-                    // Asigna las partes a variables
-                    $nombre = $parts[0];
-                    $puntos = (int)$parts[1]; // Convierte los puntos a entero
-                    $id = $parts[2];
+                        // Verifica que haya tres partes
+                        if (count($parts) == 3) {
+                            // Asigna las partes a variables
+                            $nombre = $parts[0];
+                            $puntos = (int)$parts[1]; // Convierte los puntos a entero
+                            $id = $parts[2];
 
-                    // Almacena los datos en el array
-                    $data[] = array('nombre' => $nombre, 'puntos' => $puntos, 'id' => $id);
+                            // Almacena los datos en el array
+                            $data[] = array('nombre' => $nombre, 'puntos' => $puntos, 'id' => $id);
+                        }
+                    }
+
+                    // Cierra el archivo
+                    fclose($handle);
+
+                    // Ordena el array por la columna de puntos
+                    usort($data, function($a, $b) {
+                        return $b['puntos'] - $a['puntos'];
+                    });
+
+                    // Imprime la tabla ordenada
+                    
+                    foreach ($data as $row) {
+                        echo "<tr>";
+                        echo "<td>{$row['nombre']}</td>";
+                        echo "<td>{$row['puntos']}</td>";
+                        echo "<td>{$row['id']}</td>";
+                        echo "</tr>";
+                    }
+
+                } else {
+                    echo "El archivo no existe.";
                 }
-            }
-
-            // Cierra el archivo
-            fclose($handle);
-
-            // Ordena el array por la columna de puntos
-            usort($data, function($a, $b) {
-                return $b['puntos'] - $a['puntos'];
-            });
-
-            // Imprime la tabla ordenada
-            
-            foreach ($data as $row) {
-                echo "<tr>";
-                echo "<td>{$row['nombre']}</td>";
-                echo "<td>{$row['puntos']}</td>";
-                echo "<td>{$row['id']}</td>";
-                echo "</tr>";
-            }
-
-        } else {
-            echo "El archivo no existe.";
-        }
-        ?>
+                ?>
 
 
-</table>
+        </table>
 
-    <a class="play-button" href="index.php">Volver al inicio</a>
+        <a class="play-button" href="index.php">Volver al inicio</a>
+    </div>
     </body>
 </html>
