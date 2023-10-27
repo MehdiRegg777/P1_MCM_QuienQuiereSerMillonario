d31aa186f1dc01c568a590196e3b523ecc3b1b70
cl6mh7 <claudiamoyamo@gmail.com>
2023-10-27 17:06:54 +0200

    Varios cambios

--- index.php
+++ index.php
@@ -1,41 +1,42 @@
 <?php
-session_start();
-$_SESSION['language'] = isset($_SESSION['language']) ? $_SESSION['language'] : 'spanish';
-isset($_POST['language']) ? $_SESSION['language'] = $_POST['language'] : null;
-$selectedLanguage = $_SESSION['language'];
-$hideStyle = 'style="display: none;"';
-$showStyle = 'style="display: block;"';
+    session_start();
+    $_SESSION['language'] = isset($_SESSION['language']) ? $_SESSION['language'] : 'spanish';
+    isset($_POST['language']) ? $_SESSION['language'] = $_POST['language'] : null;
+    $selectedLanguage = $_SESSION['language'];
+    $hideStyle = 'style="display: none;"';
+    $showStyle = 'style="display: block;"';
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <html>
     <head>
         <title>¿Quién quiere ser millonario?</title>
-
         <noscript>
-            <p class="noJSEnabled">¡Esta página necesita que tengas activado JavaScript para funcionar!<br />Por favor, actívalo y recarga la página para poder jugar.</p>
-
-            <style>
-                div{
-                    background-color: white;
-                    display: none;
-                    z-index: 9999;
-                }
-            </style>
+            This page needs JavaScript activated to work. 
+            <style>div { background-color: white; display:none; }</style>
         </noscript>
-
         <meta author="" content="Claudia, Mehdi i Marcelo (2n DAW)">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="style.css" rel="stylesheet">
         <link rel="shortcut icon" href="imgs/logo.png" />
     </head>
-    <body>
+    <body class="indexPage">
         <div id="spanish" <?php echo ($selectedLanguage != 'spanish') ? $hideStyle : $showStyle; ?>>
-            <header>
-                <a href="/index.php"><h1>¿Quién quiere ser millonario?</h1></a>
-            </header>
+        <header>
+            <?php
+                if ($_SESSION['language'] === 'spanish') {
+                    echo "<a href='/index.php'><h1>¿Quién quiere ser millonario?</h1></a>";
+                } elseif ($_SESSION['language'] === 'catalan') {
+                    echo "<a href='/index.php'><h1>Qui vol ser milionari?</h1></a>";
+                } elseif ($_SESSION['language'] === 'english') {
+                    echo "<a href='/index.php'><h1>Who wants to be a millonarie</h1></a>";
+                }
+            ?>
+        </header>
 
+            <!-- CATALÀ -->
             <div class="container">    
                 <h2><strong>Bienvenido</strong></h2>
                 <h3><em>Instrucciones del juego</em></h3>
@@ -88,6 +89,8 @@ $showStyle = 'style="display: block;"';
                 </div>
             </div>
         </div>
+
+        <!-- CATALÀ -->
         <div id="catalan" <?php echo ($selectedLanguage != 'catalan') ? $hideStyle : $showStyle; ?>>
             <header>
                 <h1>Qui vol ser milionari?</h1>
@@ -105,7 +108,7 @@ $showStyle = 'style="display: block;"';
                 així fins a la tercera. Si superes la primera ronda, la dificultat augmentarà. El joc SEMPRE canvia la dificultat automàticament després de
                 que l'usuari respongui a la tercera pregunta del conjunt.<br/><br /></p>
                 
-                <h3><em>Com començar a jugar?</em></hr>
+                <h3><em>Com començar a jugar?</em></h3></hr>
                 
                 <p>Primer, tens l'opció de triar l'idioma en què vols que es mostri tota la pàgina sota aquesta explicació. Quan hagis triat,
                 has de fer clic al botó <em>Jugar</em> – es situa sota aquesta explicació – per iniciar la teva partida. No es crearà un usuari llevat que
@@ -145,6 +148,8 @@ $showStyle = 'style="display: block;"';
                 </div>
             </div>
         </div>
+
+        <!-- ENGLISH -->
         <div id="english" <?php echo ($selectedLanguage != 'english') ? $hideStyle : $showStyle; ?>>
             <header>
                 <h1>Who wants to be a millionaire?</h1>
@@ -204,13 +209,15 @@ $showStyle = 'style="display: block;"';
         <audio id="start" autoplay>
             <source src="mp3/inicio.mp3" type="audio/mpeg">
         </audio>
+
+        <!-- FOOTER -->
         <footer class="footerinfo">
             <p>© MCM S.A.</p>
-            <p>Contact us</p>
-            <p>Follow us</p>
-            <p>empresa@domini.cat</p>
-            <p>twt ig p</p>
+            <p><a href="gmail.com">Contact us</a></p>
+            <p><a href="instagra.com">Follow us</a></p>
         </footer>
+
         <script src="funcionGame.js"></script>
+    
     </body>
 </html>
--- ranking.php
+++ ranking.php
@@ -1,20 +1,24 @@
 <?php
-session_start();
+    session_start();
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <html>
     <head>
         <title>¿Quién quiere ser millonario?</title>
+        <noscript>
+            This page needs JavaScript activated to work. 
+            <style>div { background-color: white; display:none; }</style>
+        </noscript>
         <meta author="" content="Claudia, Mehdi i Marcelo (2n DAW)">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="style.css" rel="stylesheet">
         <link rel="shortcut icon" href="imgs/logo.png" />
     </head>
-    <body>
+    <body class="rankingPage">
         <header>
-            <!-- IDIOMAS -->
             <?php
                 if ($_SESSION['language'] === 'spanish') {
                     echo "<a href='/index.php'><h1>¿Quién quiere ser millonario?</h1></a>";
@@ -25,36 +29,37 @@ session_start();
                 }
             ?>
         </header>
+
     <div class="container">    
         <?php
-        if ($_SESSION['language'] === 'spanish') {
-            echo '<h3>Clasificaciones</h3><br>';
-            echo '<table border="1" id="correctqueststable">';
-            echo '<tr>';
-            echo '<th>Nombre</th>';
-            echo '<th>Puntos</th>';
-            echo '<th>ID</th>';
-            echo '</tr>';  
-        } elseif ($_SESSION['language'] === 'catalan') {
-            echo '<h3>Classificacions</h3><br>';
-            echo '<table border="1" id="correctqueststable">';
-            echo '<tr>';
-            echo '<th>Nom</th>';
-            echo '<th>Punts</th>';
-            echo '<th>ID</th>';
-            echo '</tr>';
-        } elseif ($_SESSION['language'] === 'english') {
-            echo '<h3>Leaderboard</h3><br>';
-            echo '<table border="1" id="correctqueststable">';
-            echo '<tr>';
-                echo '<th>Name</th>';
-                echo '<th>Points</th>';
-                echo '<th>ID</th>';
-            echo '</tr>';
-        }      
-        ?>
+            if ($_SESSION['language'] === 'spanish') {
+                echo '<h3><em>Clasificaciones</em></h3><br>';
+                echo '<table border="1" id="correctqueststable">';
+                    echo '<tr>';
+                        echo '<th>Nombre</th>';
+                        echo '<th>Puntos</th>';
+                        echo '<th>ID</th>';
+                    echo '</tr>';  
+            
+            } elseif ($_SESSION['language'] === 'catalan') {
+                echo '<h3>Classificacions</h3><br>';
+                echo '<table border="1" id="correctqueststable">';
+                    echo '<tr>';
+                        echo '<th>Nom</th>';
+                        echo '<th>Punts</th>';
+                        echo '<th>ID</th>';
+                    echo '</tr>';
+            
+            } elseif ($_SESSION['language'] === 'english') {
+                echo '<h3>Leaderboard</h3><br>';
+                echo '<table border="1" id="correctqueststable">';
+                echo '<tr>';
+                    echo '<th>Name</th>';
+                    echo '<th>Points</th>';
+                    echo '<th>ID</th>';
+                echo '</tr>';
+            }      
 
-        <?php
             $archivo = "records.txt";
 
             if (file_exists($archivo)) {
@@ -66,35 +71,30 @@ session_start();
                     $parts = explode(",", $line);
 
                     if (count($parts) == 3) {
-
                         $nombre = $parts[0];
                         $puntos = (int)$parts[1]; 
                         $id = $parts[2];
-
                         $data[] = array('nombre' => $nombre, 'puntos' => $puntos, 'id' => $id);
-                    }
-                }
+                    }}
 
                 fclose($handle);
 
                 usort($data, function($a, $b) {
                     return $b['puntos'] - $a['puntos'];
-                });                
+                });
+
                 foreach ($data as $row) {
                     echo "<tr>";
-                    echo "<td>{$row['nombre']}</td>";
-                    echo "<td>{$row['puntos']}</td>";
-                    echo "<td>{$row['id']}</td>";
+                        echo "<td>{$row['nombre']}</td>";
+                        echo "<td>{$row['puntos']}</td>";
+                        echo "<td>{$row['id']}</td>";
                     echo "</tr>";
                 }
 
-            } else {
-                echo "El archivo no existe.";
-            }
-        ?>
+                } else { echo "El archivo no existe."; }
 
-        </table>
-        <?php
+            echo "</table>";
+                
             if ($_SESSION['language'] === 'spanish') {
                 echo '<a class="play-button" href="index.php">Volver al inicio</a>';
             } elseif ($_SESSION['language'] === 'catalan') {
@@ -102,14 +102,14 @@ session_start();
             } elseif ($_SESSION['language'] === 'english') {
                 echo '<a class="play-button" href="index.php"><em>Back to the start</em></a>';
             }
-            ?>
+        ?>
     </div>
+    
     <footer class="footerinfo">
             <p>© MCM S.A.</p>
-            <p>Contact us</p>
-            <p>Follow us</p>
-            <p>empresa@domini.cat</p>
-            <p>twt ig p</p>
-        </footer>
+            <p><a href="gmail.com">Contact us</a></p>
+            <p><a href="instagra.com">Follow us</a></p>
+    </footer>
+    
     </body>
 </html>
