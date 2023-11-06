bd5ea889a368dd6ba7422f52c70c4078c9e67ecb
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2023-11-06 11:39:00 +0100

    comitt

--- create.php
+++ create.php
@@ -23,13 +23,60 @@ session_start();
                 }
             ?>
         </header>
+        <div class="forumCreate">
+            <h2>Crear Nueva Pregunta</h2>
+            <form action="create.php" method="post">
+                <label for="idioma">Idioma:</label>
+                <select name="idioma">
+                    <option value="spanish">Español</option>
+                    <option value="catalan">Catalán</option>
+                    <option value="english">Inglés</option>
+                </select><br>
+
+                <label for="nivel">Nivel:</label>
+                <select name="nivel">
+                    <option value="1">Nivel 1</option>
+                    <option value="2">Nivel 2</option>
+                    <option value="3">Nivel 3</option>
+                    <option value="4">Nivel 4</option>
+                    <option value="5">Nivel 5</option>
+                    <option value="6">Nivel 6</option>
+                </select><br>
+
+                <label for="pregunta">Pregunta:</label>
+                <textarea name="pregunta" rows="4" cols="50"></textarea><br>
+
+                <label for="opciones">Opciones de Respuesta (separadas por líneas):</label>
+                <textarea name="opciones" rows="4" cols="50"></textarea><br>
+
+                <input type="submit" value="Crear Pregunta">
+            </form>
+        </div>
 
         <?php
-               
-            
-               
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $idioma = $_POST["idioma"];
+                $nivel = $_POST["nivel"];
+                $pregunta = $_POST["pregunta"];
+                $opciones = explode("\n", $_POST["opciones"]);
+
+
+                $nueva_pregunta = "* " . $pregunta . "\n";
+                foreach ($opciones as $opcion) {
+                    $nueva_pregunta .= ($opcion[0] == "+") ? "+ " . substr($opcion, 1) . "\n" : "- " . $opcion;
+                }
+
+                $archivo = "questions/".$idioma . "_" . $nivel . ".txt";
+
+                if (file_put_contents($archivo, $nueva_pregunta, FILE_APPEND | LOCK_EX) !== false) {
+                    echo "Pregunta creada con éxito.";
+                } else {
+                    echo "Error al crear la pregunta.";
+                }
+            } 
         ?>
-        
+
+
         <footer class='footerinfo'>
             <p>© MCM S.A.</p>
             <p><a href='gmail.com'>Contact us</a></p>
